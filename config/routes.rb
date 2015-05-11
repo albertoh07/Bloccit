f0e7bb6a97fbc8cb65ac225bca6f764a6502dfbc
albertoh07 <albertoh89@gmail.com>
2015-05-11 00:47:12 -0700

    added the contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Bloccit::Application.routes.draw do
   get "welcome/index"
   get "welcome/about"
+  get "welcome/contact"
 
-  root to: 'welcome#index'
+  #root to: 'welcome#index'
+  root to: 'welcome#contact'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
