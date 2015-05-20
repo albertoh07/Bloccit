18a430d9e845cf90c166b0d72d1486a9ef3df1df
albertoh07 <albertoh89@gmail.com>
2015-05-20 13:29:31 -0700

    Assignment 25 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,12 @@
    )
  end
  
+up = { title: 'Unique Title', body: 'Unique body'}
+Post.where(up).first_or_create
+
+up = { body: 'Unique Comment', post: Post.first }
+Comment.where(up).first_or_create
+
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
